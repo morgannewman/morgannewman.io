516d1125c245f80bf2bfa56259b2318da62c28c9
Morgan Newman <mpnewman95@gmail.com>
2018-11-06 22:12:15 -0800

    Adds introduction component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,18 @@
+import './index.scss'
 import React from 'react'
-import { Link } from 'gatsby'
+// import Typography from 'typography'
+// Components
+import Introduction from '../components/Introduction'
 
-import Layout from '../components/layout'
-import Image from '../components/image'
+// Initialize Typography rules
+// new Typography({
+//   headerFontFamily: ['Roboto Condensed', 'Avenir Next', 'sans-serif'],
+//   bodyFontFamily: ['Roboto Mono', 'Georgia', 'serif'],
+// }).injectStyles()
 
 const IndexPage = () => (
-  <Layout>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
+  <>
+    <Introduction />
+  </>
 )
-
 export default IndexPage
