5b90a8d487678dabbefcba9fb030da547329616a
Morgan Newman <mpnewman95@gmail.com>
2018-11-11 15:41:03 -0800

    Adds static version of portfolio card

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,16 +1,12 @@
 import './Portfolio.scss'
 import React from 'react'
+import PortfolioItems from './PortfolioItems'
 
 export default function Portfolio() {
   return (
     <main className="portfolio">
       <h2 className="portfolio-title">Recent Projects</h2>
-      <div className="portfolio-items">
-        <section className="portfolio-item">
-          <h3 className="emphasis portfolio-item-title">Womby</h3>
-          <img src={require('./Test.png')} className="portfolio-item-image" />
-        </section>
-      </div>
+      <PortfolioItems />
     </main>
   )
 }
