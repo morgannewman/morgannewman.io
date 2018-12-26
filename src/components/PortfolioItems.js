aba4165fab479a41a05481c7b2ca11d01dab4d0e
Morgan Newman <mpnewman95@gmail.com>
2018-12-26 12:04:23 -0800

    Adds technologies section to each project
    This is a combination of 3 commits.
    
    Updates portfolio with information about current projects
    
    Updates design to support technologies array
    
    Updates portfolio items to list technologies used

--- src/components/PortfolioItems.js
+++ src/components/PortfolioItems.js
@@ -1,17 +1,19 @@
-import './PortfolioItems.scss'
-import React from 'react'
-import Img from 'gatsby-image'
+import './PortfolioItems.scss';
+import React from 'react';
+import Img from 'gatsby-image';
 
 export default function PortfolioItems({ portfolio }) {
   return portfolio.map(item => {
-    const { title, description, screenshot, github, demo } = item
+    const { title, description, screenshot, github, demo, technologies } = item;
     return (
       <section className="portfolio-item" key={title}>
         <h3 className="emphasis portfolio-item-title">{title}</h3>
         <Img fluid={screenshot} className="portfolio-item-image" />
         <p>{description}</p>
         <div className="portfolio-item-information">
-          {/* <ul className="portfolio-item-technologies" /> */}
+          <ul className="portfolio-item-technologies">
+            {technologies.length > 0 && <li>{technologies.join(' / ')}</li>}
+          </ul>
           <div className="portfolio-item-link-container">
             <a
               href={github}
@@ -32,6 +34,6 @@ export default function PortfolioItems({ portfolio }) {
           </div>
         </div>
       </section>
-    )
-  })
+    );
+  });
 }
