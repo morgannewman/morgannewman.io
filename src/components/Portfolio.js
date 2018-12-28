ff2ab7d5aa828a52abe61e050240a92369a7ecfe
Morgan Newman <mpnewman95@gmail.com>
2018-12-28 11:37:31 -0800

    Updates text/links of portfolio items with current information

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -10,8 +10,8 @@ export default function Portfolio({ data }) {
         'Along with 3 other engineers, I helped lead the architecture and execution of a content aggregation platform called Knowted. The idea is simple: group your learning resources by topic to stay organized, and use rich text editing comparable to Evernote to keep notes on them.',
       screenshot: data.knowted.childImageSharp.fluid,
       technologies: ['JavaScript', 'React', 'Node', 'PostgreSQL'],
-      github: 'https://github.com/morgannewman/lexii',
-      demo: 'https://lexii-hack.firebaseapp.com/'
+      github: 'https://knowted.netlify.com/',
+      demo: 'https://github.com/morgannewman/knowted'
     },
     {
       title: 'Womby',
@@ -27,23 +27,23 @@ export default function Portfolio({ data }) {
       description:
         'Collaborative project with budding UX designer Matthew Lim.',
       screenshot: data.lexii.childImageSharp.fluid,
-      technologies: ['TypeScript', 'React', 'Python Flask', 'PostgreSQL'],
+      technologies: ['Python', 'TypeScript', 'React', 'PostgreSQL'],
       github: 'https://github.com/morgannewman/lexii',
-      demo: 'https://lexii-hack.firebaseapp.com/'
+      demo: 'https://lexii.netlify.com/'
     },
     {
       title: 'Lexii (Hackathon)',
       description:
-        'Built over a weekend with engineers from Facebook and Cloudflareto to help writers find relevant, royalty-free images to pair with their content—like magic.',
+        'Built over a weekend with engineers from Facebook and Cloudflare to to help writers find relevant, royalty-free images to pair with their content—like magic.',
       screenshot: data.lexiihack.childImageSharp.fluid,
-      technologies: ['JavaScript', 'React', 'Python Flask'],
-      github: 'https://github.com/morgannewman/lexii',
+      technologies: ['Python', 'JavaScript', 'React'],
+      github: 'https://github.com/morgannewman/lexii-hack',
       demo: 'https://lexii-hack.firebaseapp.com/'
     },
     {
       title: 'morgannewman.io',
       description:
-        'A daringly-simple notetaking web app that saves as you type.',
+        'Built using React, and made blazing-fast using Gatsby.js to compile down into a static site.',
       screenshot: data.portfolio.childImageSharp.fluid,
       technologies: ['JavaScript', 'React', 'Gatsby.js', 'GraphQL'],
       github: 'https://github.com/morgannewman/morgannewman.io',
@@ -55,8 +55,8 @@ export default function Portfolio({ data }) {
         'A fellow engineer and I created a playful MVP of a flashcard application to help engineers study for challenging programming interviews. The motivating factor was our curiosity to apply spaced-repetition to our own learning app.',
       screenshot: data.learnery.childImageSharp.fluid,
       technologies: ['JavaScript', 'React', 'Node', 'PostgreSQL'],
-      github: 'https://github.com/morgannewman/lexii',
-      demo: 'https://lexii-hack.firebaseapp.com/'
+      github: 'https://github.com/NikolasMsomething/learnery-frontend',
+      demo: 'https://learnery.netlify.com/'
     }
   ];
 
