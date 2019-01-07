f551a33d6a8f624beeb8b15e932d218246def1f1
Morgan Newman <mpnewman95@gmail.com>
2019-01-07 14:21:34 -0800

    Updates all the copy

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -7,13 +7,20 @@ export default function About({ data }) {
       <Img fluid={data.me.childImageSharp.fluid} className="about-img" />
       <h2 className="about-title">About Me</h2>
       <div className="about-text">
-        <p>[ PROFESSIONAL PARAGRAPH ]</p>
+        <p>
+          I am a software developer and recent graduate of UC Santa Barbara. I
+          love web development because seeing my code come to life is supremely
+          satisfying. I geek out with making my code as simple and elegant as
+          possible, and I love working with other programmers who are just as
+          passionate about the craft as I am.
+        </p>
 
         <p>
           When I’m not programming, you’ll likely find me reading, spending time
-          with my wonderful family or girlfriend, visiting friends from college
-          in SoCal, or cooking some weird Eurasian dish that requires too many
-          ingredients. You simply can’t go wrong with these things!
+          with my wonderful family or girlfriend, visiting friends all over
+          California, or cooking some unique dish that requires a mountain of
+          ingredients. I’m always up for new experiences and look forward to
+          what awaits me in the future.
         </p>
       </div>
     </section>
