b1e6fc5d33a81125f44fdc0086bedc4bae39aa81
Morgan Newman <mpnewman95@gmail.com>
2019-01-06 18:07:30 -0800

    Refactors Portfolio component to be entirely in one file

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -1,6 +1,5 @@
+import Img from 'gatsby-image';
 import React from 'react';
-
-import PortfolioItems from './PortfolioItems';
 
 export default function Portfolio({ data }) {
   const portfolio = [
@@ -53,7 +52,47 @@ export default function Portfolio({ data }) {
   return (
     <div className="portfolio">
       <h2 className="portfolio-title">Recent Projects</h2>
-      <PortfolioItems portfolio={portfolio} />
+      {portfolio.map((item) => {
+        const {
+          title,
+          description,
+          screenshot,
+          github,
+          demo,
+          technologies,
+        } = item;
+        return (
+          <section className="portfolio-item" key={title}>
+            <h3 className="emphasis portfolio-item-title">{title}</h3>
+            <Img fluid={screenshot} className="portfolio-item-image" />
+            {/* tslint:disable-next-line:jsx-key */}
+            {description.map((paragraph) => <p>{paragraph}</p>)}
+            <div className="portfolio-item-information">
+              <ul className="portfolio-item-technologies">
+                {technologies.length > 0 && <li>{technologies.join(' / ')}</li>}
+              </ul>
+              <div className="portfolio-item-link-container">
+                <a
+                  href={github}
+                  target="blank"
+                  tabIndex={1}
+                  className="portfolio-item-link portfolio-item-link-secondary"
+                >
+                  GitHub
+                </a>
+                <a
+                  href={demo}
+                  target="blank"
+                  tabIndex={1}
+                  className="portfolio-item-link portfolio-item-link-primary"
+                >
+                  Demo
+                </a>
+              </div>
+            </div>
+          </section>
+        );
+      })}
     </div>
   );
 }
