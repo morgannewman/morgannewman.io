6558b86d27958bd8125c3397e3950445cb52109b
Morgan Newman <mpnewman95@gmail.com>
2019-10-09 22:25:21 -0700

    chore: updates about section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -10,14 +10,14 @@ export default function About({ data }) {
         <p>
           I am a software developer and recent graduate of UC Santa Barbara. I
           love web development because seeing my code come to life is supremely
-          satisfying. I geek out with making my code as simple and elegant as
+          satisfying. I geek out by making my code as simple and elegant as
           possible, and I love working with other programmers who are just as
           passionate about the craft as I am.
         </p>
 
         <p>
           When I’m not programming, you’ll likely find me reading, spending time
-          with my wonderful family or girlfriend, visiting friends all over
+          with my family, visiting friends all over
           California, or cooking some unique dish that requires a mountain of
           ingredients. I’m always up for new experiences and look forward to
           what awaits me in the future.
